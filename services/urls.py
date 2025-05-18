3e766d9911c3ad99e1e11e6ee74e5b2b3fc90e48
swapglobals3333 <swapglobals@gmail.com>
2025-05-19 01:33:54 +0530

    Update urls.py

--- services/urls.py
+++ services/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('get-service/<str:pk>/', views.getService, name="get-service"),
 
     path('estimate_price/', views.calculate_estimate, name='estimate_price'),
+    path('locations/', views.locations, name='locations'),
     # path('create-service', views.createService, name="create-service"),
     # path('update-service/<str:pk>/', views.updateService, name="update-service"),
     # path('delete-service/<str:pk>/', views.deleteService, name="delete-service"),
