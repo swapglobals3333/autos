f4ec62a786b768b5498811cb156508ed8c8fea4c
swapglobals3333 <swapglobals@gmail.com>
2025-05-24 15:40:03 +0530

    Update urls.py

--- maple_auto_care/urls.py
+++ maple_auto_care/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('services/', include('services.urls')),
     path('booking/', include('booking.urls')),
 ]
-]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
